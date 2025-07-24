44d5bef16ea54199caf91601bf017558ad127056
sᴧɴᴅᴇᴇᴘ sʜᴧʀᴍᴧ <sandeepshrmaxcoder@gmail.com>
2025-07-24 17:34:14 +0530

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,10 @@
+// popup.js
+function openSandeepCard() {
+  document.querySelector(".popup-overlay").style.display = "block";
+  document.querySelector(".popup-card").style.display = "block";
+}
+
+function closeSandeepCard() {
+  document.querySelector(".popup-overlay").style.display = "none";
+  document.querySelector(".popup-card").style.display = "none";
+}
